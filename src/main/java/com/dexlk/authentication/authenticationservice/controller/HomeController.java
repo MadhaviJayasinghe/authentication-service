cb589ef1603fab10be5afdbc9146025049b61b08
MadhaviJayasinghe <lmadhavirj7512@gmail.com>
2022-05-08 15:46:40 +0530

    Implemented save credential feature

--- src/main/java/com/dexlk/authentication/authenticationservice/controller/HomeController.java
+++ src/main/java/com/dexlk/authentication/authenticationservice/controller/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-
+@RequestMapping("/")
 public class HomeController {
     @Autowired
     private JWTUtility jwtUtility;
